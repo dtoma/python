220a3879c09098f15e77658e250d2ba4d3a94cc0
Damien Toma <damien.toma@axa-im.com>
2019-03-08 18:00:09 +0800

    Add another property test

--- fisher-yates/tests.py
+++ fisher-yates/tests.py
@@ -12,9 +12,27 @@ class TestFisherYatesShuffle(unittest.TestCase):
 
     @hypothesis.given(st.lists(st.integers(), unique=True))
     @hypothesis.example([])
-    def test_simple(self, integers):
+    def test_property_length(self, integers):
+        """Test simple properties of the function.
+
+        - Length does not change after shuffling
+        - Content doesn't change after shuffling
+        """
         shuffled = shuffle.fisher_yates_shuffle(integers)
         self.assertEqual(len(integers), len(shuffled))
+        self.assertEqual(set(integers), set(shuffled))
+
+    @hypothesis.given(st.lists(st.integers(), unique=True, min_size=20))
+    def test_property_order(self, integers):
+        """Test more properties of the function.
+
+        - With larger lists, we expect that the order changes
+          after a shuffle
+        """
+        shuffled = shuffle.fisher_yates_shuffle(integers)
+        self.assertEqual(set(integers), set(shuffled))
+        self.assertNotEqual(integers, shuffled)
+
 
 
 if __name__ == '__main__':
