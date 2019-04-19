f28a7fb8353c1a7c41761b47e46165910a7b9fee
Damien Toma <damien.toma@axa-im.com>
2019-04-19 15:53:08 +0800

    Improve plots
    - better colormap
    - bar plot

--- latexreports/figures.py
+++ latexreports/figures.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 plt.style.use('seaborn')
+CMAP = plt.get_cmap('cubehelix')
 
 
 def set_size(width, fraction=1):
@@ -26,28 +27,46 @@ def set_size(width, fraction=1):
 width = 345
 
 nice_fonts = {
-        # Use LaTex to write all text
-        "text.usetex": True,
-        "font.family": "serif",
-        # Use 10pt font in plots, to match 10pt font in document
-        "axes.labelsize": 10,
-        "font.size": 10,
-        # Make the legend/label fonts a little smaller
-        "legend.fontsize": 8,
-        "xtick.labelsize": 8,
-        "ytick.labelsize": 8,
+    # Use LaTex to write all text
+    "text.usetex": True,
+    "font.family": "serif",
+    # Use 10pt font in plots, to match 10pt font in document
+    "axes.labelsize": 10,
+    "font.size": 10,
+    # Make the legend/label fonts a little smaller
+    "legend.fontsize": 8,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8,
+    # Set a colormap
+    'image.cmap': 'cubehelix',
 }
 
 mpl.rcParams.update(nice_fonts)
 
-x = np.linspace(0, 2*np.pi, 100)
-fig, ax = plt.subplots(1, 1, figsize=set_size(width))
-ax.plot(x, np.sin(x))
-ax.set_xlim(0, 2*np.pi)
-ax.set_xlabel(r'$\theta$')
-ax.set_ylabel(r'$\sin{(\theta)}$')
+# 2D bar plot
 
-plt.savefig('example_1.pdf', format='pdf', bbox_inches='tight')
+n = 12
+X = np.arange(n)
+Y1 = (1-X/float(n)) * np.random.uniform(0.5,1.0,n)
+Y2 = (1-X/float(n)) * np.random.uniform(0.5,1.0,n)
+
+fig = plt.figure(figsize=set_size(width))
+plt.axes([0.025,0.025,0.95,0.95])
+plt.bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
+plt.bar(X, -Y2, facecolor='#ff9999', edgecolor='white')
+
+for x,y in zip(X,Y1):
+    plt.text(x, y+0.05, '%.2f' % y, ha='center', va= 'bottom')
+
+for x,y in zip(X,Y2):
+    plt.text(x, -y-0.05, '%.2f' % y, ha='center', va= 'top')
+
+plt.xlim(-.5,n), plt.xticks([])
+plt.ylim(-1.25,+1.25), plt.yticks([])
+
+plt.savefig('bar_ex.pdf', format='pdf', bbox_inches='tight', dpi=48)
+
+# 3D figure
 
 fig = plt.figure(figsize=set_size(width))
 ax = Axes3D(fig)
@@ -57,9 +76,8 @@ X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
 
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.hot)
-ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.cm.hot)
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=CMAP)
+ax.contourf(X, Y, Z, zdir='z', offset=-2)
 ax.set_zlim(-2,2)
 
 plt.savefig('plot3d_ex.pdf', format='pdf', bbox_inches='tight')
-# plt.show()
