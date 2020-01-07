fc5ba33605a899358abdab8fe00de408028727b7
Damien Toma <damientoma.pro@gmail.com>
2020-01-07 21:10:05 +0800

    Add a test for another property of the btclient tokenizer

--- tests/btclient/test_btorrent_parser.py
+++ tests/btclient/test_btorrent_parser.py
@@ -45,27 +45,37 @@ class TestBTorrentTokenizer(unittest.TestCase):
         """
         self.assertEqual(list(tokenize(f"{len(text)}:{text}")), ["s", f"{text}"])
 
-    @hypothesis.given(
-        st.lists(
-            st.one_of(
-                st.builds(bencode_integer, st.integers()),
-                st.builds(bencode_bytes, st.text(min_size=1)),
-            ),
-            min_size=1,
-        )
-    )
-    def test_tokenize_list(self, ints_and_or_bytes):
+    @hypothesis.given(st.data())
+    def test_tokenize_list(self, data):
         """TODO: strategy to generate nested lists
         > A list of values is encoded as l<contents>e .
         > The contents consist of the bencoded elements of the list, in order, concatenated.
         > A list consisting of the string "spam" and the number 42 would be encoded as: l4:spami42ee.
         > Note the absence of separators between elements,
         > and the first character is the letter 'l', not digit '1'.
+
+        Test that tokenize(list) == [tokenize(elem) for elem in list].
+        Test that list1 != list2 implies tokenize(list1) != tokenize(list2)
         """
-        expected_content = itertools.chain.from_iterable(
-            tokenize(item) for item in ints_and_or_bytes
+        strategy = st.lists(
+            st.one_of(
+                st.builds(bencode_integer, st.integers()),
+                st.builds(bencode_bytes, st.text(min_size=1)),
+            ),
+            min_size=1,
         )
+
+        x = data.draw(strategy)
+        y = data.draw(strategy)
+        hypothesis.assume(x != y)
+
+        expected_content = lambda l: itertools.chain.from_iterable(
+            tokenize(item) for item in l
+        )
+
         self.assertEqual(
-            list(tokenize(f"l{''.join(ints_and_or_bytes)}e")),
-            ["l", *expected_content, "e"],
+            list(tokenize(f"l{''.join(x)}e")), ["l", *expected_content(x), "e"]
+        )
+        self.assertNotEqual(
+            list(tokenize(f"l{''.join(x)}e")), list(tokenize(f"l{''.join(y)}e"))
         )
